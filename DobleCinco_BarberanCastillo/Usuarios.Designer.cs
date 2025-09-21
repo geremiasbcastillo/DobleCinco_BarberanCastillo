35a3484a2a4cfc3c65ed048d8e9cdbee3b204586
Benjamin <geremiasbcastillo@gmail.com>
2025-09-20 21:48:09 -0300

    Formulario de usuario conectado a la base de datos

--- DobleCinco_BarberanCastillo/Usuarios.Designer.cs
+++ DobleCinco_BarberanCastillo/Usuarios.Designer.cs
@@ -30,24 +30,23 @@
         {
             LUsuarios = new Label();
             LNombre = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            TNombre = new TextBox();
+            TApellido = new TextBox();
             LApellido = new Label();
-            textBox3 = new TextBox();
+            TDni = new TextBox();
             LDNI = new Label();
-            textBox4 = new TextBox();
+            TCorreo = new TextBox();
             LCorreo = new Label();
-            textBox5 = new TextBox();
+            TContraseña = new TextBox();
             LContraseña = new Label();
-            textBox6 = new TextBox();
+            TTelefono = new TextBox();
             LTelefono = new Label();
-            textBox7 = new TextBox();
+            TDireccion = new TextBox();
             Direccion = new Label();
-            textBox8 = new TextBox();
             LFecha = new Label();
-            textBox9 = new TextBox();
-            LSexo = new Label();
             dgvUsuarios = new DataGridView();
+            BAgregar = new Button();
+            DTFecha = new DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)dgvUsuarios).BeginInit();
             SuspendLayout();
             // 
@@ -72,19 +71,19 @@
             LNombre.TabIndex = 1;
             LNombre.Text = "Nombre";
             // 
-            // textBox1
+            // TNombre
             // 
-            textBox1.Location = new Point(415, 168);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(228, 23);
-            textBox1.TabIndex = 2;
+            TNombre.Location = new Point(415, 168);
+            TNombre.Name = "TNombre";
+            TNombre.Size = new Size(228, 23);
+            TNombre.TabIndex = 2;
             // 
-            // textBox2
+            // TApellido
             // 
-            textBox2.Location = new Point(415, 235);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(228, 23);
-            textBox2.TabIndex = 4;
+            TApellido.Location = new Point(415, 235);
+            TApellido.Name = "TApellido";
+            TApellido.Size = new Size(228, 23);
+            TApellido.TabIndex = 4;
             // 
             // LApellido
             // 
@@ -96,12 +95,12 @@
             LApellido.TabIndex = 3;
             LApellido.Text = "Apellido";
             // 
-            // textBox3
+            // TDni
             // 
-            textBox3.Location = new Point(415, 301);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(228, 23);
-            textBox3.TabIndex = 6;
+            TDni.Location = new Point(415, 301);
+            TDni.Name = "TDni";
+            TDni.Size = new Size(228, 23);
+            TDni.TabIndex = 6;
             // 
             // LDNI
             // 
@@ -113,12 +112,12 @@
             LDNI.TabIndex = 5;
             LDNI.Text = "DNI";
             // 
-            // textBox4
+            // TCorreo
             // 
-            textBox4.Location = new Point(415, 366);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(228, 23);
-            textBox4.TabIndex = 8;
+            TCorreo.Location = new Point(415, 366);
+            TCorreo.Name = "TCorreo";
+            TCorreo.Size = new Size(228, 23);
+            TCorreo.TabIndex = 8;
             // 
             // LCorreo
             // 
@@ -130,128 +129,122 @@
             LCorreo.TabIndex = 7;
             LCorreo.Text = "Correo";
             // 
-            // textBox5
+            // TContraseña
             // 
-            textBox5.Location = new Point(415, 428);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(228, 23);
-            textBox5.TabIndex = 10;
+            TContraseña.Location = new Point(1087, 171);
+            TContraseña.Name = "TContraseña";
+            TContraseña.Size = new Size(228, 23);
+            TContraseña.TabIndex = 10;
             // 
             // LContraseña
             // 
             LContraseña.AutoSize = true;
             LContraseña.Font = new Font("Segoe UI", 15F);
-            LContraseña.Location = new Point(268, 423);
+            LContraseña.Location = new Point(838, 163);
             LContraseña.Name = "LContraseña";
             LContraseña.Size = new Size(110, 28);
             LContraseña.TabIndex = 9;
             LContraseña.Text = "Contraseña";
             // 
-            // textBox6
+            // TTelefono
             // 
-            textBox6.Location = new Point(1077, 168);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(228, 23);
-            textBox6.TabIndex = 12;
+            TTelefono.Location = new Point(1087, 235);
+            TTelefono.Name = "TTelefono";
+            TTelefono.Size = new Size(228, 23);
+            TTelefono.TabIndex = 12;
             // 
             // LTelefono
             // 
             LTelefono.AutoSize = true;
             LTelefono.Font = new Font("Segoe UI", 15F);
-            LTelefono.Location = new Point(838, 163);
+            LTelefono.Location = new Point(838, 230);
             LTelefono.Name = "LTelefono";
             LTelefono.Size = new Size(86, 28);
             LTelefono.TabIndex = 11;
             LTelefono.Text = "Telefono";
             // 
-            // textBox7
+            // TDireccion
             // 
-            textBox7.Location = new Point(1077, 238);
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(228, 23);
-            textBox7.TabIndex = 14;
+            TDireccion.Location = new Point(1087, 304);
+            TDireccion.Name = "TDireccion";
+            TDireccion.Size = new Size(228, 23);
+            TDireccion.TabIndex = 14;
             // 
             // Direccion
             // 
             Direccion.AutoSize = true;
             Direccion.Font = new Font("Segoe UI", 15F);
-            Direccion.Location = new Point(838, 230);
+            Direccion.Location = new Point(838, 293);
             Direccion.Name = "Direccion";
             Direccion.Size = new Size(94, 28);
             Direccion.TabIndex = 13;
             Direccion.Text = "Direccion";
             // 
-            // textBox8
-            // 
-            textBox8.Location = new Point(1077, 307);
-            textBox8.Name = "textBox8";
-            textBox8.Size = new Size(228, 23);
-            textBox8.TabIndex = 16;
-            // 
             // LFecha
             // 
             LFecha.AutoSize = true;
             LFecha.Font = new Font("Segoe UI", 15F);
-            LFecha.Location = new Point(838, 299);
+            LFecha.Location = new Point(838, 361);
             LFecha.Name = "LFecha";
             LFecha.Size = new Size(191, 28);
             LFecha.TabIndex = 15;
             LFecha.Text = "Fecha de nacimiento";
             // 
-            // textBox9
-            // 
-            textBox9.Location = new Point(1077, 366);
-            textBox9.Name = "textBox9";
-            textBox9.Size = new Size(228, 23);
-            textBox9.TabIndex = 18;
-            // 
-            // LSexo
-            // 
-            LSexo.AutoSize = true;
-            LSexo.Font = new Font("Segoe UI", 15F);
-            LSexo.Location = new Point(838, 361);
-            LSexo.Name = "LSexo";
-            LSexo.Size = new Size(54, 28);
-            LSexo.TabIndex = 17;
-            LSexo.Text = "Sexo";
-            // 
             // dgvUsuarios
             // 
             dgvUsuarios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvUsuarios.Location = new Point(353, 613);
+            dgvUsuarios.Location = new Point(353, 616);
             dgvUsuarios.Name = "dgvUsuarios";
-            dgvUsuarios.Size = new Size(240, 150);
+            dgvUsuarios.Size = new Size(798, 366);
             dgvUsuarios.TabIndex = 19;
+            // 
+            // BAgregar
+            // 
+            BAgregar.ForeColor = SystemColors.ActiveCaptionText;
+            BAgregar.Location = new Point(647, 490);
+            BAgregar.Name = "BAgregar";
+            BAgregar.Size = new Size(157, 43);
+            BAgregar.TabIndex = 20;
+            BAgregar.Text = "Agregar";
+            BAgregar.UseVisualStyleBackColor = true;
+            BAgregar.Click += BAgregar_Click;
+            // 
+            // DTFecha
+            // 
+            DTFecha.Location = new Point(1087, 366);
+            DTFecha.Name = "DTFecha";
+            DTFecha.Size = new Size(228, 23);
+            DTFecha.TabIndex = 21;
             // 
             // Usuarios
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LimeGreen;
-            ClientSize = new Size(1349, 930);
+            ClientSize = new Size(1349, 1036);
+            Controls.Add(DTFecha);
+            Controls.Add(BAgregar);
             Controls.Add(dgvUsuarios);
-            Controls.Add(textBox9);
-            Controls.Add(LSexo);
-            Controls.Add(textBox8);
             Controls.Add(LFecha);
-            Controls.Add(textBox7);
+            Controls.Add(TDireccion);
             Controls.Add(Direccion);
-            Controls.Add(textBox6);
+            Controls.Add(TTelefono);
             Controls.Add(LTelefono);
-            Controls.Add(textBox5);
+            Controls.Add(TContraseña);
             Controls.Add(LContraseña);
-            Controls.Add(textBox4);
+            Controls.Add(TCorreo);
             Controls.Add(LCorreo);
-            Controls.Add(textBox3);
+            Controls.Add(TDni);
             Controls.Add(LDNI);
-            Controls.Add(textBox2);
+            Controls.Add(TApellido);
             Controls.Add(LApellido);
-            Controls.Add(textBox1);
+            Controls.Add(TNombre);
             Controls.Add(LNombre);
             Controls.Add(LUsuarios);
             ForeColor = SystemColors.ControlLightLight;
             Name = "Usuarios";
             Text = "Form2";
+            Load += Usuarios_Load;
             ((System.ComponentModel.ISupportInitialize)dgvUsuarios).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -261,23 +254,22 @@
 
         private Label LUsuarios;
         private Label LNombre;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox TNombre;
+        private TextBox TApellido;
         private Label LApellido;
-        private TextBox textBox3;
+        private TextBox TDni;
         private Label LDNI;
-        private TextBox textBox4;
+        private TextBox TCorreo;
         private Label LCorreo;
-        private TextBox textBox5;
+        private TextBox TContraseña;
         private Label LContraseña;
-        private TextBox textBox6;
+        private TextBox TTelefono;
         private Label LTelefono;
-        private TextBox textBox7;
+        private TextBox TDireccion;
         private Label Direccion;
-        private TextBox textBox8;
         private Label LFecha;
-        private TextBox textBox9;
-        private Label LSexo;
         private DataGridView dgvUsuarios;
+        private Button BAgregar;
+        private DateTimePicker DTFecha;
     }
 }
--- DobleCinco_BarberanCastillo/Usuarios.cs
+++ DobleCinco_BarberanCastillo/Usuarios.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,11 +14,63 @@ namespace DobleCinco_BarberanCastillo
 {
     public partial class Usuarios : Form
     {
+        string connectionString = "Server=localhost;Database=doble_cinco;Trusted_Connection=True;";
+        int idSeleccionado = 0;
         public Usuarios()
         {
             InitializeComponent();
         }
 
-        
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+        }
+
+        private void CargarDatos()
+        {
+            using var conn = new SqlConnection(connectionString); // Simplifica 'using' y 'new'
+            string query = "SELECT * FROM Usuario";
+            var da = new SqlDataAdapter(query, conn); // Simplifica 'new'
+            var dt = new DataTable(); // Simplifica 'new'
+            da.Fill(dt);
+            dgvUsuarios.DataSource = dt;
+
+
+        }
+
+        private void BAgregar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Usuario (nombre_usuario, apellido_usuario, correo_usuario, telefono_usuario, dni_usuario, contraseña_usuario, direccion_usuario, id_perfil, id_estado, fecha_nacimiento_usuario) VALUES (@Nombre, @Apellido, @Correo, @Telefono, @Dni, @Contraseña, @Direccion, 1, 1, @Fecha)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Nombre", TNombre.Text);
+                cmd.Parameters.AddWithValue("@Apellido", TApellido.Text);
+                cmd.Parameters.AddWithValue("@Correo", TCorreo.Text);
+                cmd.Parameters.AddWithValue("@Telefono", TTelefono.Text);
+                cmd.Parameters.AddWithValue("@Dni", TDni.Text);
+                cmd.Parameters.AddWithValue("@Contraseña", TContraseña.Text);
+                cmd.Parameters.AddWithValue("@Direccion", TDireccion.Text);
+                cmd.Parameters.AddWithValue("@Fecha", DTFecha.Value);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            CargarDatos();
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            TNombre.Clear();
+            TApellido.Clear();
+            TCorreo.Clear();
+            TTelefono.Clear();
+            TDni.Clear();
+            TContraseña.Clear();
+            TDireccion.Clear();
+            idSeleccionado = 0;
+        }
     }
 }
